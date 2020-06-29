a3c90f039739dd453f1c25e1f85a00bf99037720
Nevin <39220032+TypicalNevin@users.noreply.github.com>
2020-06-29 15:30:38 +0200

    worked on map

--- client/reactweatherapp/src/App.js
+++ client/reactweatherapp/src/App.js
@@ -1,17 +1,105 @@
-import React from "react";
+import React, { Component } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import MapChart from "./MapChart";
 import "./style.css";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-function App() {
-  return (
-    <div className="App">
-      <MapChart />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapLocations: [],
+      mapPosition: [],
+      loading: true, // used a loading state so that map loads only after fetch request is done
+      getPos: false, // used a getPos state to check if the user has location available. If the user was on a browser like waterfox, the lat and long vars would be undefined
+    };
+  }
+
+  // function for fetching the mapdata from the openweather api
+  // it takes in a lat and lon from the getPos function
+  fetchMapData = (lat, lon) => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&units=metric&appid=7534abbbcc4e9893cbfb5684ef75fb52&cnt=10`
+      )
+      .then((res) => {
+        const {
+          data: { list },
+        } = res;
+        list.map((city) => {
+          // for each object in the array, append it to the mapLocations state
+          this.setState({
+            mapLocations: [
+              ...this.state.mapLocations,
+              {
+                cityName: city.name,
+                cityTemp: city.main.temp,
+                coords: [city.coord.lat, city.coord.lon],
+                markerOffset: -15,
+                key: uuidv4(),
+              },
+            ],
+          });
+        });
+        this.averageCoords();
+      });
+  };
+
+  averageCoords = () => {
+    let answer = [0, 0];
+
+    this.state.mapLocations.map((location) => {
+      let { coords } = location;
+      answer[0] += coords[0];
+      answer[1] += coords[1];
+    });
+
+    answer[0] = answer[0] / this.state.mapLocations.length;
+    answer[1] = answer[1] / this.state.mapLocations.length;
+    this.setState({ mapPosition: [answer[0], answer[1]] });
+  };
+
+  getCurrentPos = () => {
+    if ("geolocation" in navigator) {
+      // check if user has geolocation enabled
+      navigator.geolocation.getCurrentPosition((pos) => {
+        this.setState({ getPos: true }); // set state of both the mapPos and mapPosition
+        this.fetchMapData(pos.coords.latitude, pos.coords.longitude); // fetch map data
+      });
+    } else {
+      alert("Please enable location");
+    }
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      // set a timeout to allow the openweather api to respond with data
+      if (this.state.getPos === true) this.setState({ loading: false });
+      // check if location is valid
+      else {
+        alert("Please allow location");
+        window.location.reload();
+      }
+    }, 150);
+    this.getCurrentPos();
+  }
+
+  render() {
+    return (
+      !this.state.loading && (
+        <div className="App">
+          <MapChart
+            mapLocations={this.state.mapLocations}
+            mapPosition={this.state.mapPosition}
+          />
+        </div>
+      )
+    );
+  }
 }
 
 export default App;
--- client/reactweatherapp/src/MapChart.js
+++ client/reactweatherapp/src/MapChart.js
@@ -1,146 +1,31 @@
 import React, { Component } from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import axios from "axios";
 import "./style.css";
-
-const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-
-const something = [52.16, 4.37];
 
 class MapChart extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      mapLocations: [],
-    };
-  }
-
-  fetchMapData = () => {
-    axios
-      .get(
-        "https://api.openweathermap.org/data/2.5/find?lat=52.163379199999994&lon=4.3778048&units=metric&appid=7534abbbcc4e9893cbfb5684ef75fb52&cnt=10"
-      )
-      .then((res) => {
-        const {
-          data: { list },
-        } = res;
-        list.map((city) => {
-          this.setState({
-            mapLocations: [
-              ...this.state.mapLocations,
-              {
-                cityName: city.name,
-                cityTemp: city.main.temp,
-                coords: [city.coord.lon, city.coord.lat],
-                markerOffset: -15,
-              },
-            ],
-          });
-        });
-        console.log(this.state.mapLocations);
-      });
-  };
-
-  componentDidMount() {
-    this.fetchMapData();
   }
 
   render() {
     return (
-      <div>
-        {setTimeout(() => {}, 5000)}
-        <Map center={something} zoom={10}>
+      <div className="main-map border border-secondary">
+        <Map center={this.props.mapPosition} zoom={12}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={something}>
-            <Popup>Nevin</Popup>
-          </Marker>
+          {this.props.mapLocations.map((location) => (
+            <Marker position={location.coords} key={location.key}>
+              <Popup>
+                {location.cityName} | {location.cityTemp}
+              </Popup>
+            </Marker>
+          ))}
         </Map>
-        ;
       </div>
     );
   }
 }
 
 export default MapChart;
-
-/*
-  fetchMapData = () => {
-    axios
-      .get(
-        "https://api.openweathermap.org/data/2.5/find?lat=52.163379199999994&lon=4.3778048&units=metric&appid=7534abbbcc4e9893cbfb5684ef75fb52&cnt=10"
-      )
-      .then((res) => {
-        const {
-          data: { list },
-        } = res;
-        list.map((city) => {
-          this.setState({
-            mapLocations: [
-              ...this.state.mapLocations,
-              {
-                cityName: city.name,
-                cityTemp: city.main.temp,
-                coords: { lat: city.coord.lat, long: city.coord.lon },
-              },
-            ],
-          });
-        });
-        console.log(this.state.mapLocations);
-      });
-  };
-
-*/
-
-/*
-
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  Marker,
-} from "react-simple-maps";
-
-  render() {
-    return (
-      <div>
-        <ComposableMap
-          projection="geoAzimuthalEqualArea"
-          projectionConfig={{ rotate: [-4.3, -52.1, 0], scale: 80000 }}
-        >
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill="#EAEAEC"
-                  stroke="#D6D6DA"
-                />
-              ))
-            }
-          </Geographies>
-          {this.state.mapLocations.map(({ cityName, coords, markerOffset }) => (
-            <Marker key={cityName} coordinates={coords}>
-              <circle r={4} fill="#1ce206" stroke="#636363" strokeWidth={2} />
-              <text
-                textAnchor="middle"
-                y={markerOffset}
-                fontSize="5px"
-                style={{ fontFamily: "comic sans ms", fill: "#7662e5" }}
-              >
-                {cityName}
-              </text>
-            </Marker>
-          ))}
-        </ComposableMap>
-        <h1>fasdfasdf</h1>
-      </div>
-    );
-  }
-
-
-*/
